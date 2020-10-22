a098a31a865dfc950b0b07ebd3654a37bfe53b50
bubblesort <aleksanrmerklin@gmail.com>
2020-10-22 14:36:08 +0400

    block keyboard and a little validation

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -3,9 +3,14 @@ const isTable = document.querySelector('main').dataset.table;
 if (isTable) {
   const ALL_KEYBOARD = document.querySelectorAll('.keyboard__td');
   const form = document.querySelector('.calculator__form');
+  const input = document.querySelector('#expression');
 
   ALL_KEYBOARD.forEach(key => {
     key.addEventListener('click', touchKeyboard)
+  })
+
+  input.addEventListener('input', evt => {
+    input.value = input.value.replace(/[A-Za-zА-Яа-яЁё]/, '')
   })
 
 
@@ -13,9 +18,9 @@ if (isTable) {
   form.addEventListener('submit', evt => {
     const inputField = document.querySelector('#expression');
 
-
     console.log(typeof inputField.value);
     if (!inputField.value) {
+      evt.preventDefault()
       console.log('hool')
     }
   })
@@ -24,10 +29,15 @@ if (isTable) {
     const touchedKey = evt.target.dataset.keyboard;
     const inputField = document.querySelector('#expression');
     const lastValue = inputField.value.slice(-1);
-    let prevNumber;
+    console.log(lastValue);
+    let prevNumber = false;
     inputField.focus()
+    if (touchedKey === 'delete-all') {
+      inputField.value = '';
+      return;
+    }
     if (touchedKey === 'delete') {
-      inputField.value = inputField.value.slice(0, -1)
+      inputField.value = inputField.value.slice(0, -1);
       return;
     }
 
@@ -47,18 +57,32 @@ if (isTable) {
       case ',':
         prevNumber = false;
         break;
+      case '':
+        prevNumber = false;
+        break;
       default:
         prevNumber = true;
         break;
     }
-    if (checkDoubleOperation(prevNumber, touchedKey)) {
+    if (checkDoubleOperation(prevNumber, touchedKey, lastValue)) {
       inputField.value +=  touchedKey;
     }
   }
 
-  function checkDoubleOperation(prevNumber, touchedKey) {
+  function checkDoubleOperation(prevNumber, touchedKey, lastValue) {
     if (prevNumber === false) {
-      if ((touchedKey === '÷') || (touchedKey === '+') || (touchedKey === '−') || (touchedKey === '×') || (touchedKey === ',')) {
+      if ((lastValue === '') && (touchedKey === ')' || touchedKey === '(')) {
+        return true;
+      }
+      if (
+        (touchedKey === '÷') ||
+        (touchedKey === '+') ||
+        (touchedKey === '−') ||
+        (touchedKey === '×') ||
+        (touchedKey === ',') ||
+        (touchedKey === ')') ||
+        (touchedKey === '(')
+      ) {
         return false;
       }
     }
