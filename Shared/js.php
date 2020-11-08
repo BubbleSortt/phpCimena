27f6ff274278e8179466fce49dbf7008af884b17
bubblesort <aleksanrmerklin@gmail.com>
2020-11-08 14:07:45 +0400

    i hope that dont crashed

--- Shared/js.php
+++
@@ -1,1 +0,0 @@
-<script src="../static/app.92058d132dbd9bfcd8d8.js"></script>
--- Shared/links.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Все линки в одном месте
-?>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Anton&family=Work+Sans&display=swap">
-<link href="../static/app.92058d132dbd9bfcd8d8.css" rel="stylesheet"></head>
-<title><?php $config['title']?></title>
